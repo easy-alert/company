27bd7354063f7f258f08d4b3d79ed01a5c157e4a
jorgelda <jorgeluiz112233@gmail.com>
2022-11-16 12:58:55 -0300

    develop

--- src/screens/NotificationsConfirmation/Email/styles.tsx
+++ src/screens/NotificationsConfirmation/Email/styles.tsx
@@ -13,15 +13,69 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: ${theme.size.xxlg};
-
-  > img {
-    width: 100%;
-    height: 87px;
-    max-width: 400px;
-  }
 
   @media (max-width: 900px) {
     padding: ${theme.size.sm};
+  }
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 350px;
+  z-index: 2;
+
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: ${theme.size.xlg};
+
+  > img {
+    height: 89px;
+    width: 79px;
+  }
+`;
+
+export const ContentText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+
+  > :nth-child(3) {
+    margin-top: ${theme.size.sm};
+  }
+
+  p {
+    color: #3f3e3e;
+  }
+
+  span {
+    color: ${theme.color.primary};
+    word-break: break-all;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+
+  z-index: 1;
+
+  > img {
+    width: 100%;
+    max-width: 450px;
+
+    height: 450px;
+  }
+
+  @media (max-width: 900px) {
+    display: none;
   }
 `;
