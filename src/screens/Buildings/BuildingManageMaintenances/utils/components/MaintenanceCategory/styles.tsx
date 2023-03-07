da75fbeb237c27bb59a8e0ba12a2abe224fc5ea0
jorgelda <jorgeluiz112233@gmail.com>
2023-03-07 16:36:53 -0300

    develop -

--- src/screens/Buildings/BuildingManageMaintenances/utils/components/MaintenanceCategory/styles.tsx
+++ src/screens/Buildings/BuildingManageMaintenances/utils/components/MaintenanceCategory/styles.tsx
@@ -58,7 +58,7 @@ export const MaintenancesGrid = styled.div`
   grid-template-rows: 1fr;
   align-items: center;
   grid-gap: ${theme.size.sm};
-  grid-template-columns: 8px 230px minmax(230px, 0.5fr) 0.6fr 0.4fr 0.5fr 95px 60px;
+  grid-template-columns: 8px 230px minmax(230px, 0.5fr) 0.6fr 0.4fr 0.5fr 60px 60px;
 `;
 
 export const SortHeader = styled.div<{ highlighted: boolean }>`
