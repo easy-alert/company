8d129159d047985755377e3d4f0fb577495a1470
jorgelda <jorgeluiz112233@gmail.com>
2023-05-29 11:34:52 -0300

    SA-3867 -

--- src/screens/Reports/Create/functions.ts
+++ src/screens/Reports/Create/functions.ts
@@ -14,7 +14,7 @@ export const requestReportsData = async ({
   setMaintenances([]);
 
   await Api.get(
-    `/buildings/reports/list?maintenanceStatusId=${filters.maintenanceStatusId}&buildingId=${filters.buildingIds}&categoryId=${filters.categoryId}&startDate=${filters.startDate}&endDate=${filters.endDate}`,
+    `/buildings/reports/list?maintenanceStatusIds=${filters.maintenanceStatusId}&buildingIds=${filters.buildingIds}&categoryIds=${filters.categoryId}&startDate=${filters.startDate}&endDate=${filters.endDate}`,
   )
     .then((res) => {
       setMaintenances(res.data.maintenances);
