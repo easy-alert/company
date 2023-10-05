c9a9a3913eb15a372d67966131616c8ca821d324
jorgelda <jorgeluiz112233@gmail.com>
2023-10-05 14:59:19 -0300

    SA-4537 - fix inprogress name

--- src/components/InProgressTag/index.tsx
+++ src/components/InProgressTag/index.tsx
@@ -3,6 +3,6 @@ import * as Style from './styles';
 
 export const InProgressTag = () => (
   <Style.TagContainer>
-    <p className="p7">Em andamento</p>
+    <p className="p7">Em execução</p>
   </Style.TagContainer>
 );
