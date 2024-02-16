bfcc886f9948b4fe66da5d7ff5b1a7e8c780eb5d
jorgelda <jorgeluiz112233@gmail.com>
2024-02-16 15:33:04 -0300

    develop - fix pdf

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,9 +1,11 @@
 import backgroundForPDF from './backgroundForPDF.png';
 import logoForPDF from './logoForPDF.png';
 import logoForPDF2 from './logoForPDF2.png';
+import imagePlaceholder from './imagePlaceholder.png';
 
 export const image = {
   backgroundForPDF,
   logoForPDF,
   logoForPDF2,
+  imagePlaceholder,
 };
