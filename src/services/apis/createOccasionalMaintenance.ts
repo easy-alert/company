504991641a266ec72ac0246eacff55dd38d7344b
Guilherme Roos <guilherme.roosr@gmail.com>
2024-11-01 09:55:52 -0300

    feat: add optional ticketsIds parameter to createOccasionalMaintenance and handle empty selection case

--- src/services/apis/createOccasionalMaintenance.ts
+++ src/services/apis/createOccasionalMaintenance.ts
@@ -13,6 +13,8 @@ interface IRequestCreateOccasionalMaintenance {
   origin: string;
   occasionalMaintenanceType: IOccasionalMaintenanceType;
   occasionalMaintenanceBody: IOccasionalMaintenanceData;
+
+  ticketsIds?: string[];
 }
 
 interface IResponseCreateOccasionalMaintenance extends IResponse {
@@ -29,6 +31,7 @@ interface IResponseCreateOccasionalMaintenance extends IResponse {
 
 export const createOccasionalMaintenance = async ({
   origin,
+  ticketsIds = [],
   occasionalMaintenanceType,
   occasionalMaintenanceBody: {
     buildingId,
@@ -42,6 +45,14 @@ export const createOccasionalMaintenance = async ({
   },
 }: IRequestCreateOccasionalMaintenance) => {
   const uri = '/buildings/reports/occasional/create';
+  if (ticketsIds.length === 0) {
+    handleToastify({
+      statusCode: 400,
+      message: 'Nenhum chamado selecionado.',
+    });
+
+    return null;
+  }
 
   try {
     const response: IResponseCreateOccasionalMaintenance = await Api.post(uri, {
@@ -65,6 +76,7 @@ export const createOccasionalMaintenance = async ({
         files: reportData.files || null,
         images: reportData.images || null,
       },
+      ticketsIds,
     });
 
     handleToastify({
