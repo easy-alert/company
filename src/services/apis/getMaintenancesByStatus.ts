9577302a09ea5531ef80d160dcdcbf907d6542ba
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-07 17:39:04 -0300

    feat: add API service to fetch maintenances by status with error handling

---
+++ src/services/apis/getMaintenancesByStatus.ts
@@ -0,0 +1,34 @@
+import { Api } from '@services/api';
+
+import { handleToastify } from '@utils/toastifyResponses';
+
+import type { IDashboardFilter } from '@screens/Dashboard';
+
+export const getMaintenancesByStatus = async (
+  periodFilter: string,
+  dashboardFilter: IDashboardFilter,
+  resetFilters?: boolean,
+) => {
+  const uri = '/dashboard/maintenances/status';
+
+  const params = {
+    period: periodFilter,
+    buildings: resetFilters ? JSON.stringify([]) : JSON.stringify(dashboardFilter.buildings),
+    categories: resetFilters ? JSON.stringify([]) : JSON.stringify(dashboardFilter.categories),
+    responsible: resetFilters ? JSON.stringify([]) : JSON.stringify(dashboardFilter.responsible),
+  };
+
+  try {
+    const response = await Api.get(uri, { params });
+
+    return response.data;
+  } catch (error: any) {
+    handleToastify(error.response.data.ServerMessage);
+
+    return {
+      data: [],
+      labels: [],
+      colors: [],
+    };
+  }
+};
