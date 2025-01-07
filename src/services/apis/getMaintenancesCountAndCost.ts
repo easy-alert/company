6b213e0c8d7cd975eb8eaa1e90476f1f90218f0c
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-07 17:39:59 -0300

    feat: add API service to fetch maintenance count and cost with error handling

---
+++ src/services/apis/getMaintenancesCountAndCost.ts
@@ -0,0 +1,35 @@
+import { Api } from '@services/api';
+
+import { handleToastify } from '@utils/toastifyResponses';
+
+import type { IDashboardFilter } from '@screens/Dashboard';
+
+export const getMaintenancesCountAndCost = async (
+  periodFilter: string,
+  dashboardFilter: IDashboardFilter,
+  maintenanceType: 'common' | 'occasional' | '',
+  resetFilters?: boolean,
+) => {
+  const uri = '/dashboard/maintenances/count-and-cost';
+
+  const params = {
+    period: periodFilter,
+    buildings: resetFilters ? JSON.stringify([]) : JSON.stringify(dashboardFilter.buildings),
+    categories: resetFilters ? JSON.stringify([]) : JSON.stringify(dashboardFilter.categories),
+    responsible: resetFilters ? JSON.stringify([]) : JSON.stringify(dashboardFilter.responsible),
+    maintenanceType,
+  };
+
+  try {
+    const response = await Api.get(uri, { params });
+
+    return response.data;
+  } catch (error: any) {
+    handleToastify(error.response.data.ServerMessage);
+
+    return {
+      maintenanceCount: 0,
+      maintenanceCost: 'Valor investido R$ 0,00',
+    };
+  }
+};
