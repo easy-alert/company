04a9c2e226719060a50fa9a90c9796f91d8f76a8
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-07 17:41:08 -0300

    feat: add API service to fetch tickets count and cost with error handling

---
+++ src/services/apis/getTicketsCountAndCost.ts
@@ -0,0 +1,36 @@
+import { Api } from '@services/api';
+
+import { handleToastify } from '@utils/toastifyResponses';
+
+import type { IDashboardFilter } from '@screens/Dashboard';
+import type { ITicketStatusNames } from '@customTypes/ITicket';
+
+export const getTicketsCountAndCost = async (
+  periodFilter: string,
+  dashboardFilter: IDashboardFilter,
+  ticketStatus: ITicketStatusNames | '',
+  resetFilters?: boolean,
+) => {
+  const uri = '/dashboard/tickets/count-and-cost';
+
+  const params = {
+    period: periodFilter,
+    buildings: resetFilters ? JSON.stringify([]) : JSON.stringify(dashboardFilter.buildings),
+    categories: resetFilters ? JSON.stringify([]) : JSON.stringify(dashboardFilter.categories),
+    responsible: resetFilters ? JSON.stringify([]) : JSON.stringify(dashboardFilter.responsible),
+    ticketStatus,
+  };
+
+  try {
+    const response = await Api.get(uri, { params });
+
+    return response.data;
+  } catch (error: any) {
+    handleToastify(error.response.data.ServerMessage);
+
+    return {
+      maintenanceCount: 0,
+      maintenanceCost: 'Valor investido R$ 0,00',
+    };
+  }
+};
