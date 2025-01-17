ff2613414f193f3122dcc0e593a991b15414fdc2
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-17 00:24:56 -0300

    feat: add PermissionRoute component for permission-based routing

---
+++ src/routes/PermissionRoute.tsx
@@ -0,0 +1,25 @@
+import { useContext } from 'react';
+
+import { Navigate, Outlet } from 'react-router-dom';
+
+import { AuthContext } from '@contexts/Auth/AuthContext';
+
+interface IPermissionRoute {
+  requiredPermission: string;
+}
+
+const PermissionRoute = ({ requiredPermission }: IPermissionRoute) => {
+  const { account } = useContext(AuthContext);
+
+  // if (
+  //   !account?.User ||
+  //   !account?.User.Permissions?.some((perm) => perm.Permission.name === requiredPermission) ||
+  //   !account?.User.Permissions?.find((perm) => perm.Permission.name.includes('admin'))
+  // ) {
+  //   return <Navigate to="/forbidden" replace />;
+  // }
+
+  return <Outlet />;
+};
+
+export default PermissionRoute;
