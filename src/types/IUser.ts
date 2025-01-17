9406ee9ab9f5e76b94f387a191f65e17640b4a46
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-17 00:23:35 -0300

    feat: add IPermission interface and update IUser interface to include permissions

--- src/types/IUser.ts
+++ src/types/IUser.ts
@@ -1,8 +1,19 @@
+import type { IPermission } from './IPermission';
+
 export interface IUser {
   id: string;
   name: string;
   email: string;
   phone: string;
+
+  Permissions: {
+    Permission: IPermission;
+    id: string;
+    permissionId: string;
+    userId: string;
+    createdAt: string;
+    updatedAt: string;
+  }[];
 
   isBlocked: boolean;
 
