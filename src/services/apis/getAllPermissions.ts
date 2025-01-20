7880a5863a80e81c4e988ae1fe44388c8c5cbf28
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-20 16:06:17 -0300

    feat: enhance getAllPermissions API to filter out backoffice permissions and modules

--- src/services/apis/getAllPermissions.ts
+++ src/services/apis/getAllPermissions.ts
@@ -2,8 +2,21 @@ import { Api } from '@services/api';
 
 import { handleToastify } from '@utils/toastifyResponses';
 
+import type { IPermission } from '@customTypes/IPermission';
+import type { IResponse } from '@customTypes/IResponse';
+
 interface IGetAllPermissions {
   adminPermissions: boolean;
+}
+
+interface IResponseAllPermissions extends IResponse {
+  data: {
+    permissions: IPermission[];
+    modules: {
+      moduleName: string;
+      moduleLabel: string;
+    }[];
+  };
 }
 
 export const getAllPermissions = async ({ adminPermissions }: IGetAllPermissions) => {
@@ -14,9 +27,20 @@ export const getAllPermissions = async ({ adminPermissions }: IGetAllPermissions
   };
 
   try {
-    const response = await Api.get(uri, { params });
+    const response: IResponseAllPermissions = await Api.get(uri, { params });
 
-    return response.data;
+    const permissions = response.data.permissions.filter(
+      (permission) => !permission.name.includes('backoffice'),
+    );
+
+    const modules = response.data.modules.filter(
+      (module) => !module.moduleName.includes('backoffice'),
+    );
+
+    return {
+      permissions,
+      modules,
+    };
   } catch (error: any) {
     handleToastify(error.response.data.ServerMessage);
 
