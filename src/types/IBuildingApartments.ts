3ecd9b320df5960d613f4e42cb668702bc10596e
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-23 18:21:30 -0300

    feat: add IBuildingApartment interface for building apartment types

---
+++ src/types/IBuildingApartments.ts
@@ -0,0 +1,9 @@
+export interface IBuildingApartment {
+  id?: number;
+  buildingId?: string;
+  companyId?: string;
+  number?: string;
+  floor?: number;
+  createdAt?: string;
+  updatedAt?: string;
+}
