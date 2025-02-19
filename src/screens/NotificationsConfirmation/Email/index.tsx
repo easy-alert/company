3edf0db6c7f1e156f6ac5c5ed7b353451da86399
Guilherme Roos <guilherme.roosr@gmail.com>
2025-02-19 14:33:45 -0300

    feat: implement email confirmation logic and update UI for user confirmation

--- src/screens/NotificationsConfirmation/Email/index.tsx
+++ src/screens/NotificationsConfirmation/Email/index.tsx
@@ -1,13 +1,15 @@
 // STYLES
 import { Link, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { getBuildingByToken } from '@services/apis/getBuildingByToken';
+import { confirmUser } from '@services/apis/confirmUser';
 import * as Style from './styles';
 
 // COMPONENTS
 import { Button } from '../../../components/Buttons/Button';
 
 // FUNCTIONS
-import { requestConfirmData, requestGetBuildingName } from '../utils/functions';
+import { requestConfirmData } from '../utils/functions';
 import { query } from '../../../utils/functions';
 import { DotSpinLoading } from '../../../components/Loadings/DotSpinLoading';
 
@@ -17,15 +19,47 @@ export const EmailConfirm = () => {
 
   const [loading, setLoading] = useState<boolean>(true);
 
-  const [onQuery, setOnQuery] = useState<boolean>(false);
-
   const [isConfirmed, setIsConfirmed] = useState<boolean>(false);
-  const [buildingName, setBuildingName] = useState<string>('');
+  const [companyName, setCompanyName] = useState<string>('');
   const [companyImage, setCompanyImage] = useState<string>('');
 
+  const handleConfirmUser = async () => {
+    if (!token) {
+      return;
+    }
+
+    try {
+      await confirmUser({ token });
+      setIsConfirmed(true);
+    } catch {
+      navigate('/login');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    requestGetBuildingName({ setBuildingName, setCompanyImage, setLoading, token: token! });
-  }, []);
+    const handleGetBuildingByToken = async () => {
+      if (!token) {
+        return;
+      }
+
+      try {
+        const responseData = await getBuildingByToken({ token });
+
+        const { name, image } = responseData;
+
+        setCompanyName(name);
+        setCompanyImage(image);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (token) {
+      handleGetBuildingByToken();
+    }
+  }, [token]);
 
   return loading ? (
     <Style.Container>
@@ -39,33 +73,24 @@ export const EmailConfirm = () => {
           <h2>Cadastro de e-mail confirmado!</h2>
 
           <Style.ContentText>
-            <p>Agora você é responsável pela edificação:</p>
-            <span> {buildingName}</span>
+            <p>Você é um usuário confirmado da empresa:</p>
+            <span> {companyName}</span>
           </Style.ContentText>
         </Style.Content>
       ) : (
         <Style.Content>
           <img src={companyImage} alt="" />
 
-          <h2>Confirmação de responsável</h2>
+          <h2>Confirmação de usuário</h2>
 
           <Style.ContentText>
-            <p>Seu e-mail foi cadastrado na Easy Alert para ser o responsável da edificação:</p>
-            <span> {buildingName}</span>
+            <p>
+              Seu e-mail foi cadastrado na Easy Alert para ser um usuário confirmado da empresa:
+            </p>
+            <span> {companyName}</span>
             <p>Para confirmar seu cadastro clique no botão abaixo:</p>
             <Style.BottomContainer>
-              <Button
-                loading={onQuery}
-                label="Confirmar"
-                onClick={() => {
-                  requestConfirmData({
-                    token: token!,
-                    setIsConfirmed,
-                    navigate,
-                    setOnQuery,
-                  });
-                }}
-              />
+              <Button loading={loading} label="Confirmar" onClick={handleConfirmUser} />
 
               <span>
                 Ao clicar em confirmar, você concorda com os{' '}
