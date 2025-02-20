8408537775de698c1329242045719777135eb2e5
Guilherme Roos <guilherme.roosr@gmail.com>
2025-02-20 17:53:07 -0300

    feat: add API functions for managing checklists and maintenance kanban

---
+++ src/services/apis/putChecklist.ts
@@ -0,0 +1,29 @@
+import { IChecklistItem } from '@customTypes/IChecklistItem';
+import { Api } from '@services/api';
+
+import { handleToastify } from '@utils/toastifyResponses';
+
+interface IPutChecklist {
+  checklistId: string;
+  checklistItems: IChecklistItem[];
+  status: string;
+  images?: any[];
+}
+
+export async function putChecklist({ checklistId, checklistItems, status, images }: IPutChecklist) {
+  const uri = `/checklists/${checklistId}`;
+
+  const body = {
+    checklistItems,
+    status,
+    images,
+  };
+
+  try {
+    const response = await Api.put(uri, body);
+
+    handleToastify(response);
+  } catch (error: any) {
+    handleToastify(error.response);
+  }
+}
