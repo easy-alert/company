e3abb6b67707df50a6c4d7bdb0dc08af7b115723
Guilherme Roos <guilherme.roosr@gmail.com>
2025-03-12 15:08:56 -0300

    Revert "Merge branches 'feat/SE-199/delete-checklist-templates' and 'feat/SE-199/delete-checklist-templates' of https://gitlab.com/new-easy-alert/company into feat/SE-199/delete-checklist-templates"
    This reverts commit 5cfec87ce26c992228d2f529425075c472e07b6e, reversing
    changes made to 29c6d296513054b254f47a3bcf27108a2e790618.

--- src/screens/Checklists/ModalChecklistCreate/ModelTemplate/index.tsx
+++ src/screens/Checklists/ModalChecklistCreate/ModelTemplate/index.tsx
@@ -4,7 +4,6 @@ import { IconButton } from '@components/Buttons/IconButton';
 import { icon } from '@assets/icons';
 import type { IChecklistTemplate } from '@customTypes/IChecklistTemplate';
 import { deleteChecklist } from '@services/apis/deleteChecklist';
-
 import * as Style from './styles';
 
 interface ModelTemplateProps {
@@ -41,23 +40,19 @@ export const ModelTemplate = ({
       <Style.SelectTitle>Selecione um template</Style.SelectTitle>
       {checklistTemplates.length > 0 ? (
         checklistTemplates.map((template) => (
-          <Style.TemplateContainer
-            key={template.id}
-            onClick={() => {
-              setSelectedTemplate({
-                id: template.id,
-                name: template.name,
-                items: template.items,
-              });
+          <Style.TemplateContainer key={template.id}>
+            {selectedTemplate?.id === template.id && (
+              <Image img={icon.checked} size="16px" alt="Checked Icon" />
+            )}
 
-              handleSelectChecklistTemplate({
-                id: template.id,
-                name: template.name,
-                items: template.items,
-              });
-            }}
-          >
-            <Style.TemplateOption>{template.name}</Style.TemplateOption>
+            <Style.TemplateOption
+              onClick={() => {
+                setSelectedTemplate(template);
+                handleSelectChecklistTemplate(template);
+              }}
+            >
+              {template.name}
+            </Style.TemplateOption>
 
             <IconButton
               icon={icon.trashWithPrimaryBg}
