60a0ce1350b11edf77c2554c2a8427f545e91bc3
Marcelli <matesmarcelli@gmail.com>
2025-04-03 09:36:04 -0300

    feat: tests on the password recovery screen

---
+++ cypress/e2e/recoverPassword-page.cy.ts
@@ -0,0 +1,39 @@
+describe('Tests in Recover password', () => {
+  beforeEach(() => {
+    cy.visit('/passwordrecover/sendemail');
+  });
+  it('must visit and show login registration', () => {
+    cy.getByTestId('company-logo').should('be.visible');
+  });
+
+  describe('Send password recovery email', () => {
+    it('should fill the email field', () => {
+      cy.getByTestId('email-input').should('be.visible').type('matesmarcelli@gmail.com');
+      cy.getByTestId('toSend-button').should('be.visible').click();
+      cy.url().should('include', '/login');
+      cy.getByTestId('login-container').should('be.visible');
+    });
+
+    it('should show an error when the email does not exist in the database', () => {
+      cy.getByTestId('email-input').should('be.visible').type('marcelli@easyalrt.com.br');
+      cy.getByTestId('toSend-button').click();
+    });
+
+    it('should submit the form with the correct data', () => {
+      cy.getByTestId('toSend-button').should('be.visible').click();
+      cy.getByTestId('email-error').should('contain.text', 'E-mail obrigatório.');
+    });
+  });
+  it('should show an error message when the email is invalid', () => {
+    cy.getByTestId('email-input').should('be.visible').type('marcelli@easyalert');
+    cy.getByTestId('toSend-button').click();
+    cy.getByTestId('email-error').should('contain.text', 'Informe um e-mail válido.');
+  });
+  describe('Redirect to other page', () => {
+    it('should redirect to the login page', () => {
+      cy.getByTestId('login-link').should('be.visible').click();
+      cy.url().should('include', '/login');
+      cy.getByTestId('login-container').should('be.visible');
+    });
+  });
+});
