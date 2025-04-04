fbb8b22b0f8380c81273d4154e066e8b394fc6f7
Guilherme Roos <guilherme.roosr@gmail.com>
2025-04-04 09:33:08 -0300

    feat: enhance Modal component with edit functionality and customizable zIndex

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,22 +1,20 @@
 // GLOBAL ASSETS
-import IconX from '@assets/icons/IconX';
-import { icon } from '@assets/icons';
+import { IconButton } from '@components/Buttons/IconButton';
+import { ShareMaintenanceHistoryButton } from '@components/ShareMaintenanceHistoryButton';
 
 // GLOBAL COMPONENTS
+import IconX from '@assets/icons/IconX';
 import IconTrash from '@assets/icons/IconTrash';
-import { IconButton } from '../Buttons/IconButton';
+import IconEdit from '@assets/icons/IconEdit';
 
-// GLOBAL STYLES
-import { theme } from '../../styles/theme';
+// GLOBAL UTILS
+import { query } from '@utils/functions';
 
-// GLOBAL UTILS / FUNCTIONS
-import { query } from '../../utils/functions';
-
-// COMPONENTS
+// STYLES
 import * as Style from './styles';
 
 // TYPES
-import { IModal } from './utils/types';
+import type { IModal } from './utils/types';
 
 export const Modal = ({
   id = 'background',
@@ -24,12 +22,15 @@ export const Modal = ({
   title,
   bodyWidth,
   closeOutside = true,
-  deleteButton = false,
+  maintenanceHistoryId,
+  zIndex = 10,
   setModal,
   handleDelete,
+  handleEdit,
 }: IModal) => (
   <Style.Background
     id={id}
+    zIndex={zIndex}
     onMouseDown={(evt: any) => {
       if (evt.target.id === id && !query.get('flow') && closeOutside) setModal(false);
     }}
@@ -39,11 +40,23 @@ export const Modal = ({
         <h2>{title}</h2>
 
         <Style.IconsContainer>
-          {deleteButton && (
+          {handleEdit && (
+            <IconButton
+              icon={<IconEdit strokeColor="primary" strokeWidth="2" />}
+              permToCheck="maintenances:updateDates"
+              onClick={() => handleEdit(true)}
+            />
+          )}
+
+          {handleDelete && (
             <IconButton
               icon={<IconTrash strokeColor="primary" strokeWidth="2" />}
-              onClick={() => handleDelete && handleDelete(true)}
+              onClick={() => handleDelete(true)}
             />
+          )}
+
+          {maintenanceHistoryId && (
+            <ShareMaintenanceHistoryButton maintenanceHistoryId={maintenanceHistoryId} />
           )}
 
           <IconButton
