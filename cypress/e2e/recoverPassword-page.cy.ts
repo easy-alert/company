24a7f885b24a1679b8c4968959686888e9263544
Marcelli <matesmarcelli@gmail.com>
2025-04-08 10:48:30 -0300

    feat: fixed test descriptions, and add change requested by review

--- cypress/e2e/recoverPassword-page.cy.ts
+++ cypress/e2e/recoverPassword-page.cy.ts
@@ -2,14 +2,17 @@ describe('Tests in Recover password', () => {
   beforeEach(() => {
     cy.visit('/passwordrecover/sendemail');
   });
+
   it('must visit and show login registration', () => {
     cy.getByTestId('company-logo').should('be.visible');
   });
 
   describe('Send password recovery email', () => {
-    it('should fill the email field', () => {
-      cy.getByTestId('email-input').should('be.visible').type('matesmarcelli@gmail.com');
+    it('should send recovery email successfully', () => {
+      cy.getByTestId('email-input').should('be.visible').type('company@gmail.com');
       cy.getByTestId('toSend-button').should('be.visible').click();
+      
+      cy.checkToastMessage('success-toast', 'E-mail de recuperação de senha enviado com sucesso.');
       cy.url().should('include', '/login');
       cy.getByTestId('login-container').should('be.visible');
     });
@@ -17,18 +20,21 @@ describe('Tests in Recover password', () => {
     it('should show an error when the email does not exist in the database', () => {
       cy.getByTestId('email-input').should('be.visible').type('marcelli@easyalrt.com.br');
       cy.getByTestId('toSend-button').click();
+      cy.checkToastMessage('error-toast', 'A informação: usuário não existe na base de dados.');
     });
 
-    it('should submit the form with the correct data', () => {
+    it('should show error when submitting without filling email', () => {
       cy.getByTestId('toSend-button').should('be.visible').click();
       cy.getByTestId('email-error').should('contain.text', 'E-mail obrigatório.');
     });
   });
+
   it('should show an error message when the email is invalid', () => {
     cy.getByTestId('email-input').should('be.visible').type('marcelli@easyalert');
     cy.getByTestId('toSend-button').click();
     cy.getByTestId('email-error').should('contain.text', 'Informe um e-mail válido.');
   });
+
   describe('Redirect to other page', () => {
     it('should redirect to the login page', () => {
       cy.getByTestId('login-link').should('be.visible').click();
