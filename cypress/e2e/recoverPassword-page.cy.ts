cb4dda76a0405c3d7cc9e163d320f5a38e56fa5b
Guilherme Roos <guilherme.roosr@gmail.com>
2025-04-09 10:44:16 -0300

    refactor: reorganize error handling tests in Recover Password component

--- cypress/e2e/recoverPassword-page.cy.ts
+++ cypress/e2e/recoverPassword-page.cy.ts
@@ -7,14 +7,24 @@ describe('Tests in Recover password', () => {
     cy.getByTestId('company-logo').should('be.visible');
   });
 
+  it('should show an error when submitting without filling the email field', () => {
+    cy.getByTestId('toSend-button').should('be.visible').click();
+    cy.getByTestId('email-error').should('contain.text', 'E-mail obrigatório.');
+  });
+
+  it('should show an error message when the email is invalid', () => {
+    cy.getByTestId('email-input').should('be.visible').type('marcelli@easyalert');
+    cy.getByTestId('toSend-button').click();
+    cy.getByTestId('email-error').should('contain.text', 'Informe um e-mail válido.');
+  });
+
   describe('Send password recovery email', () => {
     it('should send recovery email successfully', () => {
       cy.getByTestId('email-input').should('be.visible').type('company@gmail.com');
       cy.getByTestId('toSend-button').should('be.visible').click();
-
+      cy.checkToastMessage('success-toast', 'E-mail de recuperação de senha enviado com sucesso.');
       cy.url().should('include', '/login');
       cy.getByTestId('login-container').should('be.visible');
-      cy.checkToastMessage('success-toast', 'E-mail de recuperação de senha enviado com sucesso.');
     });
 
     it('should show an error when the email does not exist in the database', () => {
@@ -22,17 +32,6 @@ describe('Tests in Recover password', () => {
       cy.getByTestId('toSend-button').click();
       cy.checkToastMessage('error-toast', 'A informação: usuário não existe na base de dados.');
     });
-
-    it('should show an error when submitting without filling the email field', () => {
-      cy.getByTestId('toSend-button').should('be.visible').click();
-      cy.getByTestId('email-error').should('contain.text', 'E-mail obrigatório.');
-    });
-  });
-
-  it('should show an error message when the email is invalid', () => {
-    cy.getByTestId('email-input').should('be.visible').type('marcelli@easyalert');
-    cy.getByTestId('toSend-button').click();
-    cy.getByTestId('email-error').should('contain.text', 'Informe um e-mail válido.');
   });
 
   describe('Redirect to other page', () => {
