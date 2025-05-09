e88f9beeaa3650c58fadc36193fe88b89ced59af
Marcelli <marcelli@easyalert.com.br>
2025-05-09 13:43:46 -0300

    feat: fixed api function

---
+++ src/services/apis/putReportPdf.ts
@@ -0,0 +1,21 @@
+import { Api } from '@services/api';
+import { handleToastify } from '@utils/toastifyResponses';
+
+interface IRequestUpdateReportName {
+  reportId: string;
+  reportType: 'ticket' | 'maintenance';
+  reportName: string;
+}
+
+export async function putReportPdf({ reportId, reportType, reportName }: IRequestUpdateReportName) {
+  const uri = `/report/${reportId}?reportType=${reportType}`;
+  const body = { reportName };
+
+  try {
+    const response = await Api.put(uri, body);
+
+    handleToastify(response);
+  } catch (error: any) {
+    handleToastify(error.response);
+  }
+}
