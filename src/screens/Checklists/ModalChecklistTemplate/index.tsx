4839db182394ca0dbb6c6eba6c24d0c857ff16cb
Guilherme Roos <guilherme@easyalert.com.br>
2025-05-12 17:45:03 -0300

    feat: implement ModalChecklistTemplate component with checklist management functionality

---
+++ src/screens/Checklists/ModalChecklistTemplate/index.tsx
@@ -0,0 +1,227 @@
+import { useEffect, useState } from 'react';
+
+// SERVICES
+import { getChecklistsTemplates } from '@services/apis/getChecklistsTemplates';
+import { deleteChecklistTemplate } from '@services/apis/deleteChecklistTemplate';
+
+// GLOBAL COMPONENTS
+import { Button } from '@components/Buttons/Button';
+import { ImagePreview } from '@components/ImagePreview';
+import { Modal } from '@components/Modal';
+import { InputCheckbox } from '@components/Inputs/InputCheckbox';
+import { LoadingWrapper } from '@components/Loadings/LoadingWrapper';
+import { DotSpinLoading } from '@components/Loadings/DotSpinLoading';
+import { DotLoading } from '@components/Loadings/DotLoading';
+import { InputRadio } from '@components/Inputs/InputRadio';
+import UserResponsible from '@components/UserResponsible';
+import { IconButton } from '@components/Buttons/IconButton';
+
+// GLOBAL UTILS
+import { uploadManyFiles } from '@utils/functions';
+
+// GLOBAL ASSETS
+import IconTrash from '@assets/icons/IconTrash';
+
+// GLOBAL TYPES
+import { IChecklistTemplate } from '@customTypes/IChecklistTemplate';
+import { IChecklist, TChecklistStatus } from '@customTypes/IChecklist';
+
+// STYLES
+import IconEdit from '@assets/icons/IconEdit';
+import { Input } from '@components/Inputs/Input';
+import IconPlus from '@assets/icons/IconPlus';
+import { putChecklistTemplateById } from '@services/apis/putChecklistTemplateById';
+import * as Style from './styles';
+
+import type { TModalNames } from '..';
+
+interface ModalSendReportProps {
+  handleModals: (modal: TModalNames, modalState: boolean) => void;
+}
+
+export const ModalChecklistTemplate = ({ handleModals }: ModalSendReportProps) => {
+  const [checklistTemplates, setChecklistTemplates] = useState<IChecklistTemplate[]>([]);
+  const [selectedTemplate, setSelectedTemplate] = useState<IChecklistTemplate | null>(null);
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [refresh, setRefresh] = useState<boolean>(false);
+
+  const handleRefresh = () => {
+    setRefresh((prev) => !prev);
+  };
+
+  const handleGetChecklistsTemplate = async () => {
+    setLoading(true);
+
+    try {
+      const responseData = await getChecklistsTemplates({ buildingId: '' });
+      setChecklistTemplates(responseData);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleUpdateChecklistTemplate = async () => {
+    if (!selectedTemplate) return;
+
+    setLoading(true);
+
+    try {
+      const responseData = await putChecklistTemplateById({
+        ...selectedTemplate,
+      });
+      console.log('🚀 ~ handleUpdateChecklistTemplate ~ responseData:', responseData);
+    } finally {
+      setLoading(false);
+      setSelectedTemplate(null);
+      handleRefresh();
+    }
+  };
+
+  const handleDeleteChecklistTemplate = async (templateId: string) => {
+    try {
+      const responseData = await deleteChecklistTemplate({ checklistTemplateId: templateId });
+
+      if (responseData) {
+        setChecklistTemplates((prevTemplates) =>
+          prevTemplates.filter((template) => template.id !== templateId),
+        );
+
+        if (selectedTemplate?.id === templateId) {
+          setSelectedTemplate(null);
+        }
+      }
+    } catch (error) {
+      console.error('Erro ao deletar o template:', error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetChecklistsTemplate();
+  }, [refresh]);
+
+  return (
+    <Modal
+      title={selectedTemplate ? 'Editar modelo' : 'Modelos de checklist'}
+      setModal={(modalState) => handleModals('modalChecklistTemplate', modalState)}
+    >
+      {loading ? (
+        <LoadingWrapper minHeight="300px">
+          <DotSpinLoading />
+        </LoadingWrapper>
+      ) : (
+        <Style.Content>
+          {!selectedTemplate && (
+            <>
+              <Style.SelectTitle>Selecione um template</Style.SelectTitle>
+
+              <div>
+                {checklistTemplates.length > 0 ? (
+                  checklistTemplates.map((template) => (
+                    <Style.TemplateContainer key={template.id}>
+                      <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <Style.TemplateOption>{template.name}</Style.TemplateOption>
+                      </div>
+
+                      <div style={{ display: 'flex', gap: '0.5rem' }}>
+                        <IconButton
+                          icon={<IconEdit strokeColor="primary" />}
+                          zIndex={1}
+                          onClick={() => {
+                            setSelectedTemplate(template);
+                          }}
+                        />
+
+                        <IconButton
+                          icon={<IconTrash strokeColor="danger" />}
+                          onClick={async () => {
+                            if (template.id) {
+                              await handleDeleteChecklistTemplate(template.id);
+                            }
+                          }}
+                        />
+                      </div>
+                    </Style.TemplateContainer>
+                  ))
+                ) : (
+                  <p>Nenhum template foi salvo até o momento.</p>
+                )}
+              </div>
+            </>
+          )}
+
+          {selectedTemplate && (
+            <>
+              <Input
+                label=""
+                value={selectedTemplate.name}
+                placeholder="Título do checklist"
+                onChange={(e) => setSelectedTemplate({ ...selectedTemplate, name: e.target.value })}
+                autoFocus
+                onFocus={(e) => e.target.select()}
+              />
+
+              <Style.ChecklistContainer>
+                {selectedTemplate.items?.map((item, index) => (
+                  <Style.ChecklistItem key={item.id}>
+                    <Input
+                      label=""
+                      type="text"
+                      value={item.name}
+                      onChange={(e) =>
+                        setSelectedTemplate((prev) => {
+                          const newItems = [...(prev?.items || [])];
+                          newItems[index].name = e.target.value;
+                          return { ...prev, items: newItems };
+                        })
+                      }
+                    />
+
+                    <IconButton
+                      icon={<IconTrash strokeColor="danger" />}
+                      onClick={() =>
+                        setSelectedTemplate((prev) => {
+                          const newItems = [...(prev?.items || [])];
+                          newItems.splice(index, 1);
+                          return { ...prev, items: newItems };
+                        })
+                      }
+                    />
+                  </Style.ChecklistItem>
+                ))}
+
+                <div>
+                  <IconButton
+                    icon={<IconPlus strokeColor="success" />}
+                    onClick={() =>
+                      setSelectedTemplate((prev) => {
+                        const newItems = [...(prev?.items || [])];
+                        newItems.push({ id: newItems.length.toString(), name: '' });
+                        return { ...prev, items: newItems };
+                      })
+                    }
+                  />
+                </div>
+
+                <Style.ChecklistButtons>
+                  <Button
+                    label="Cancelar"
+                    color="danger"
+                    borderless
+                    onClick={() => setSelectedTemplate(null)}
+                  />
+
+                  <Button
+                    label="Salvar"
+                    bgColor="success"
+                    onClick={handleUpdateChecklistTemplate}
+                  />
+                </Style.ChecklistButtons>
+              </Style.ChecklistContainer>
+            </>
+          )}
+        </Style.Content>
+      )}
+    </Modal>
+  );
+};
---
+++ src/screens/Checklists/ModalChecklistTemplate/styles.tsx
@@ -0,0 +1,105 @@
+import styled from 'styled-components';
+import { theme } from '@styles/theme';
+
+export const Content = styled.div`
+  padding: ${theme.size.sm};
+  background-color: #f9f9f9;
+  border-radius: ${theme.size.xsm};
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  max-width: 100%; /* Ensure the content does not exceed the modal width */
+  word-wrap: break-word; /* Break long words */
+`;
+
+export const SelectTitle = styled.p`
+  font-size: 14px;
+  color: ${theme.color.gray5};
+  font-weight: 500;
+  margin-bottom: ${theme.size.xsm};
+`;
+
+export const TemplateContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+
+  padding-left: ${theme.size.xsm};
+  padding-right: ${theme.size.xsm};
+
+  cursor: pointer;
+  border-radius: 4px;
+  transition: background-color 0.2s, color 0.2s;
+
+  &:hover {
+    background-color: ${theme.color.gray2};
+  }
+
+  &:active {
+    background-color: ${theme.color.gray3};
+  }
+`;
+
+export const TemplateOption = styled.p`
+  font-size: 14px;
+  color: ${theme.color.gray6};
+  padding: ${theme.size.xsm};
+`;
+
+export const SelectedTemplate = styled.div`
+  margin-top: ${theme.size.sm};
+  padding: ${theme.size.sm};
+  background-color: ${theme.color.gray2};
+  border-radius: ${theme.size.xsm};
+`;
+
+export const SelectedTemplateTitle = styled.p`
+  font-size: 14px;
+  color: ${theme.color.gray5};
+  font-weight: 500;
+  margin-bottom: ${theme.size.xsm};
+`;
+
+export const SelectedTemplateTasks = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SelectedTemplateTask = styled.p`
+  font-size: 14px;
+  color: ${theme.color.gray6};
+  margin-bottom: ${theme.size.xsm};
+`;
+
+export const ChecklistTitle = styled.div`
+  margin-left: ${theme.size.xsm};
+  word-wrap: break-word;
+
+  > p {
+    font-size: ${theme.size.sm};
+    font-weight: 600;
+    word-wrap: break-word;
+  }
+`;
+
+export const ChecklistContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.size.xxsm};
+
+  margin-top: ${theme.size.xsm};
+  margin-left: ${theme.size.md};
+`;
+
+export const ChecklistItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${theme.size.xsm};
+`;
+
+export const ChecklistButtons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: ${theme.size.sm};
+`;
