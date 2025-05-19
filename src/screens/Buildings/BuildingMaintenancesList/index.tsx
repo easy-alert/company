9a064b49ae34106ab906f1bff5b967690ca73de5
Guilherme Roos <guilherme@easyalert.com.br>
2025-05-19 09:38:18 -0300

    feat: enable CSV export functionality in BuildingMaintenancesList component

--- src/screens/Buildings/BuildingMaintenancesList/index.tsx
+++ src/screens/Buildings/BuildingMaintenancesList/index.tsx
@@ -134,7 +134,7 @@ export const BuildingMaintenancesList = () => {
           </Style.LeftSide>
 
           <Style.RightSide>
-            {/* <CSVLink
+            <CSVLink
               data={csvData}
               headers={csvHeaders}
               filename={`Plano de manutenção ${new Date().toLocaleDateString('pt-BR')}`}
@@ -149,7 +149,7 @@ export const BuildingMaintenancesList = () => {
                   //
                 }}
               />
-            </CSVLink> */}
+            </CSVLink>
 
             <IconButton
               label="Exportar"
