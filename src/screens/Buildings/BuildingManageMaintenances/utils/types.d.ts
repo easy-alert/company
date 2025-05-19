c711a4c269335086fdea4a49bf10e02f45149c38
Guilherme Roos <guilherme@easyalert.com.br>
2025-05-19 14:55:43 -0300

    fix: remove trailing whitespace in IRequestManageBuildingMaintenances interface

--- src/screens/Buildings/BuildingManageMaintenances/utils/types.d.ts
+++ src/screens/Buildings/BuildingManageMaintenances/utils/types.d.ts
@@ -76,7 +76,7 @@ export interface IRequestManageBuildingMaintenances {
   categories: ICategories[];
   buildingId: string;
   setOnQuery: (setOnQuery: boolean) => void;
-  origin: 'Company' | 'Backoffice' | 'Client' | undefined; 
+  origin: 'Company' | 'Backoffice' | 'Client' | undefined;
 }
 
 export interface IBuildingListForSelect {
