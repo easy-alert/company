de4507527ab6e21bd477b56dc80fe87bfc09e586
Guilherme Roos <guilherme@easyalert.com.br>
2025-05-29 11:34:25 -0300

    Update title logic in UserResponsible component for better user count handling

--- src/components/UserResponsible/index.tsx
+++ src/components/UserResponsible/index.tsx
@@ -14,7 +14,8 @@ interface IUserResponsible {
 }
 
 const UserResponsible = ({ users }: IUserResponsible) => {
-  const title = users?.length === 1 ? 'Usuário Responsável' : 'Usuários Responsáveis';
+  const title =
+    users?.length === 0 || users?.length === 1 ? 'Usuário Responsável' : 'Usuários Responsáveis';
 
   if (!users) {
     return null;
