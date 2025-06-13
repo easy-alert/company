5598abc2166f5478766d73fb2ebd8ef325f2523c
Guilherme Roos <guilherme@easyalert.com.br>
2025-06-13 11:54:34 -0300

    fix: make handleRefresh optional in IModalMaintenanceReportSend interface

--- src/components/MaintenanceModals/ModalMaintenanceReportSend/types.d.ts
+++ src/components/MaintenanceModals/ModalMaintenanceReportSend/types.d.ts
@@ -4,5 +4,5 @@ export interface IModalMaintenanceReportSend {
   maintenanceHistoryId: string;
   refresh: boolean;
   handleModals: (modal: TModalNames, modalState: boolean) => void;
-  handleRefresh: () => void;
+  handleRefresh?: () => void;
 }
