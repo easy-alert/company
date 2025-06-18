cff8d6b5e8d87fc45ab71911b3e999ce0803a879
Guilherme Roos <guilherme@easyalert.com.br>
2025-06-18 17:31:24 -0300

    Add maintenanceToggleProgress API service for updating maintenance status

---
+++ src/services/apis/maintenanceToggleProgress.ts
@@ -0,0 +1,27 @@
+import { Api } from '@services/api';
+
+import { handleToastify } from '@utils/toastifyResponses';
+
+interface IMaintenanceToggleProgress {
+  userId: string;
+  maintenanceHistoryId: string;
+  inProgressChange: boolean;
+}
+
+export const maintenanceToggleProgress = async ({userId, maintenanceHistoryId, inProgressChange}:IMaintenanceToggleProgress) => {
+  const uri = `/maintenances/set/in-progress`;
+
+  const body = {
+    userId,
+    maintenanceHistoryId,
+    inProgressChange,
+  };
+
+  try {
+    const response = await Api.post(uri, body);
+
+    handleToastify(response);
+  } catch (error: any) {
+    handleToastify(error.response, false);
+  }
+};
