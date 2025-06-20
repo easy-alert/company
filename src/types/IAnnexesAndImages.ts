0f7ad808958a58b7ca65abcd9e843ad6a7b0a649
Guilherme Roos <guilherme@easyalert.com.br>
2025-06-20 17:10:13 -0300

    Enhance IAnnexesAndImages interface to include optional id field and extend IAnnexesAndImagesWithActivityId with activityId

--- src/types/IAnnexesAndImages.ts
+++ src/types/IAnnexesAndImages.ts
@@ -1,6 +1,12 @@
 export interface IAnnexesAndImages {
+  id?: string;
+
   name: string;
   originalName: string;
   url: string;
   type?: string;
+}
+
+export interface IAnnexesAndImagesWithActivityId extends IAnnexesAndImages {
+  activityId?: string;
 }
