648baad305dcc0e333b2544794e29c394bd97d0e
Marcelli <marcelli@easyalert.com.br>
2025-06-26 13:55:06 -0300

    feat: new desing

--- src/screens/Authentication/SendPasswordRecoverEmail/styles.tsx
+++ src/screens/Authentication/SendPasswordRecoverEmail/styles.tsx
@@ -1,19 +1,24 @@
 import styled from 'styled-components';
-import { theme } from '../../../styles/theme';
+import { theme } from '@styles/theme';
+import bannerLogin from '@assets/images/bannerLogin.jpg';
 
 export const Background = styled.div`
+  width: 100vw;
   min-height: 100vh;
   display: flex;
-  align-items: center;
-  flex-direction: column;
   justify-content: center;
-  padding: ${theme.size.sm} ${theme.size.md};
-  background-color: #3f3e3e;
+  align-items: center;
 
-  > img {
-    width: 100%;
-    max-width: 290px;
-    height: 65px;
+  background-image: url(${bannerLogin});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    min-height: 100vh;
+    height: auto;
+    padding: 0;
   }
 
   > p {
@@ -57,5 +62,58 @@ export const LoginContainer = styled.div`
 
   button {
     margin-top: ${theme.size.xsm};
+  }
+`;
+
+export const LeftSide = styled.div`
+  flex: 0.6;
+`;
+
+export const RightSide = styled.div`
+  flex: 0.4;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100vh;
+  align-items: center;
+  gap: ${theme.size.md};
+  background-color: ${theme.color.primary};
+  padding: ${theme.size.xlg} ${theme.size.lg};
+  border-top-right-radius: 12px;
+  border-bottom-right-radius: 12px;
+
+  h2 {
+    font-size: 18px;
+    color: ${theme.color.white};
+    text-align: center;
+    margin-bottom: ${theme.size.md};
+  }
+
+  > img {
+    width: 80%;
+    max-width: 300px;
+    margin-bottom: ${theme.size.md};
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: ${theme.size.md};
+  }
+
+  button {
+    margin-top: ${theme.size.sm};
+    background-color: white;
+    color: ${theme.color.primary};
+  }
+
+  @media (max-width: 768px) {
+    border-radius: 0;
+    flex: none;
+    width: 100%;
+    height: 100%;
+    padding: ${theme.size.md};
   }
 `;
