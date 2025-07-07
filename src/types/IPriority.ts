42b7221d059a52f3776d618c435e42839726396b
Guilherme Roos <guilherme@easyalert.com.br>
2025-07-07 10:40:11 -0300

    feat: update IPriority interface to use TPriorityName and TPriorityNameLabel types

--- src/types/IPriority.ts
+++ src/types/IPriority.ts
@@ -1,6 +1,8 @@
+import type { TPriorityName, TPriorityNameLabel } from './TPriorityName';
+
 export interface IPriority {
-  name?: string;
-  label?: string;
+  name?: TPriorityName;
+  label?: TPriorityNameLabel;
   color?: string;
   backgroundColor?: string;
 }
