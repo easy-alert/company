7d92a78ac1a3d59d5b0ab75601bbbbe868bb37ab
Guilherme Roos <guilherme@easyalert.com.br>
2025-08-04 16:38:31 -0300

    feat: add styles props to Image component

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -6,8 +6,16 @@ import * as Style from './styles';
 // TYPES
 import { ImageProps } from './utils/types';
 
-export const Image = ({ img, radius = '100%', size = '48px', width, height, alt }: ImageProps) => (
-  <Style.ImgHolder radius={radius} size={size} width={width} height={height}>
+export const Image = ({
+  img,
+  radius = '100%',
+  size = '48px',
+  width,
+  height,
+  alt,
+  style,
+}: ImageProps) => (
+  <Style.ImgHolder radius={radius} size={size} width={width} height={height} style={style}>
     {typeof img === 'string' ? (
       <img src={img} alt={alt ?? ''} />
     ) : (
