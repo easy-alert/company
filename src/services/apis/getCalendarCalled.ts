2348f90d19df867dc07fa05193fb9292a9f86be0
Marcelli <marcelli@easyalert.com.br>
2025-08-05 11:54:43 -0300

    feat: calendar-called API call

---
+++ src/services/apis/getCalendarCalled.ts
@@ -0,0 +1,30 @@
+// GLOBAL API
+import { Api } from '@services/api';
+
+// GLOBAL UTILS
+import { handleToastify } from '@utils/toastifyResponses';
+
+// GLOBAL TYPES
+import type {
+  IGetCalendarCalledParams,
+  IResponseGetCalendarCalled,
+} from '@customTypes/ICalendarCalled';
+
+export const getCalendarCalled = async ({
+  companyId,
+  year,
+  buildingId,
+}: IGetCalendarCalledParams) => {
+  const uri = `calendar-called/list/${year}?companyId=${companyId}&buildingId=${buildingId}`;
+
+  try {
+    const response: { data: IResponseGetCalendarCalled } = await Api.get(uri);
+    return response.data;
+  } catch (error: any) {
+    handleToastify(error);
+    return {
+      Days: [],
+      buildings: [],
+    };
+  }
+};
