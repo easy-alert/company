0f8972acb5256cddcb48e348b1086493351848bc
Guilherme Roos <guilherme@easyalert.com.br>
2025-08-22 12:56:52 -0300

    feat: add stock interfaces

---
+++ src/types/IStockItem.ts
@@ -0,0 +1,61 @@
+import type { IBuilding } from './IBuilding';
+import type { IStockItemType } from './IStockItemType';
+
+export interface IStockItem {
+  id: string;
+
+  companyId?: string;
+  buildingId?: string;
+  stockItemTypeId?: string;
+
+  name?: string;
+  description?: string;
+  unit?: string;
+  isActive?: boolean;
+
+  createdAt?: string;
+  updatedAt?: string;
+
+  // company: ICompany;
+  building?: IBuilding;
+  stockItemType?: IStockItemType;
+
+  // stocks: IStock[];
+  // movements: IStockMovement[];
+}
+
+export interface IStockItemForm {
+  name: string;
+  description?: string;
+  unit: string;
+  stockItemTypeId: string;
+  isActive: boolean;
+}
+
+// model StockItem {
+//   id String @id @default(uuid())
+
+//   companyId       String
+//   buildingId      String?
+//   stockItemTypeId String
+
+//   name        String
+//   description String?
+//   unit        String
+//   isActive    Boolean @default(true)
+
+//   createdAt DateTime @default(now())
+//   updatedAt DateTime @updatedAt
+
+//   company       Company       @relation(fields: [companyId], references: [id], onDelete: Cascade)
+//   building      Building?     @relation(fields: [buildingId], references: [id], onDelete: Cascade)
+//   stockItemType StockItemType @relation(fields: [stockItemTypeId], references: [id])
+
+//   stocks    Stock[]
+//   movements StockMovement[]
+
+//   @@unique([companyId, name, buildingId])
+//   @@index([companyId])
+//   @@index([buildingId])
+//   @@map("stockItems")
+// }
