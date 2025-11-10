9a575c42265b1b8e300bdf2bde4cbc084e9e3a20
Gabriel Castelan <gabrielmcastelan@gmail.com>
2025-11-10 14:53:08 -0300

    feat: add image URL validation and placeholder support for thumbnail

--- src/components/ImageComponent/index.tsx
+++ src/components/ImageComponent/index.tsx
@@ -1,4 +1,5 @@
 // COMPONENTS
+import { isValidImageUrl } from '@utils/isValidImageURL';
 import * as Style from './styles';
 import { IImg } from './types';
 
@@ -11,15 +12,27 @@ export const ImageComponent = ({
   radius = '0%',
   rotate = '0deg',
   hasCircle,
-}: IImg) => (
-  <Style.Img
-    src={src}
-    alt={alt}
-    radius={radius}
-    size={size}
-    width={width}
-    height={height}
-    rotate={rotate}
-    hasCircle={hasCircle}
-  />
-);
+}: IImg) => {
+  let displaySrc = src;
+  if (!isValidImageUrl(src)) {
+    // tenta extrair o tipo do arquivo da url
+    const matches = src.match(/\.([a-zA-Z0-9]+)(?:\?.*)?$/);
+    const filetype = matches ? matches[1] : 'Anexo';
+    // limita texto para ser exibido legalmente no placeholder
+    const text = encodeURIComponent(filetype.length > 10 ? filetype.slice(0, 10) : filetype);
+    displaySrc = `https://placehold.co/150?text=${text}&font=roboto`;
+  }
+
+  return (
+    <Style.Img
+      src={displaySrc}
+      alt={alt}
+      radius={radius}
+      size={size}
+      width={width}
+      height={height}
+      rotate={rotate}
+      hasCircle={hasCircle}
+    />
+  );
+};
